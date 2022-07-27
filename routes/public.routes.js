e02e0fb2ffb8298bc568e92dcdc7adbd7d69e4c8
Sebastian Laca <seba.laca@gmail.com>
2022-07-27 21:37:48 +0200

    27/07 ultimo commit

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const Cat = require("../models/Cat.model.js")
 const Dog = require("../models/Dog.model.js")
-const Contact = require("../models/Contact.js")
+const Contact = require("../models/Contact.model.js")
 const Stories= require("../models/Stories.model.js")
 const Map =require("../models/Map.model.js")
 
